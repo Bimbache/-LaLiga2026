7212dd5e810a1a8843f09a2568a7e4019a66b1b8
Bimbache <misterpupas@gmail.com>
2026-06-23 01:04:31 +0100

    Update odds.js

--- api/odds.js
+++ api/odds.js
@@ -1,1 +1,13 @@
 
+export default async function handler(req, res) {
+  const { sport, regions, markets } = req.query;
+
+  try {
+    const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds?apiKey=${process.env.ODDS_API_KEY}&regions=${regions}&markets=${markets}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
